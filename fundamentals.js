13f93d6fd0a334c889b3f417ffb45070c96ff23b
Vincent L Sweeney Jr <vincentswee@gmail.COM>
2017-08-18 10:18:32 -0400

    jscheckpointvs

--- fundamentals.js
+++ fundamentals.js
@@ -2,27 +2,32 @@
 
 // #1: Create an array of strings called `colors` that contains three colors.
 // Type your solution immediately below this line:
-
+var colors = ["blue", "red", "green"];
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+var name = last [2];
 
 
 // #3: Create an empty array called `favoriteColors`.
 // Type your solution immediately below this line:
-
+var favoriteColors = {}
 
 
 // #4: Create a `for` loop that adds each string in `colors` to `favoriteColors`.
 // Type your solution immediately below this line:
-
-
+<p id="info"></p>
+var text = "";
+var i ;
+for (i = 0;  i <favoriteColors.lengh; i++) {
+  colors+= favoriteColors [i] + "<br>"
+}
+document.getElementById("info").innerHTML = text;
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+var instructor = {height:"normal", weight:"normal", color"blue"};
 
 
 // #6: Add a `facial-hair` (spelled exactly) property to `instructor` by accessing
